193a5fc7571193b883b6cf52c72fa02ed7903150
Nicolas Taillepierre <taillepierren@gmail.com>
2022-01-12 14:20:27 +0100

    clean/ajout commentaire product

--- front/js/product.js
+++ front/js/product.js
@@ -10,7 +10,7 @@ let cart = [];
 getProduct();
 addToCart();
 
-
+//Recuperer les donné du back
 function getProduct() {
     fetch("http://localhost:3000/api/products/" + id)
         .then(function (gotProduct) {
@@ -24,7 +24,7 @@ function getProduct() {
             console.log(err);
         })
 
-
+// modification du DOM avec les donnée du back
 
         .then(function (gotCanape) {
 
@@ -57,46 +57,34 @@ function getProduct() {
 
 };
 
-function createItem(){
-    let nbrArticle = document.getElementById('quantity').value;
-    let choosenColor = document.getElementById('colors').value;
-    let DOMimg = document.getElementById('canapimg');
-
-    cartItem = {
-        _id: id,
-        colors: choosenColor,
-        name: DOMtitle.innerHTML,
-        price: DOMprice.innerHTML,
-        imageUrl: DOMimg.src,
-        description: DOMdesc.innerHTML,
-        altTxt: DOMimg.alt,
-        nbrArticle: nbrArticle,
-    }
-    cart.push(cartItem);
-    localStorage.setItem("storedcart", JSON.stringify(cart));
-}
-
+//function lancé au click bouton "ajout au panier"
 function addToCart() {
     addToCartBtn.addEventListener("click", function () {
 
         let nbrArticle = document.getElementById('quantity').value;
         let choosenColor = document.getElementById('colors').value;
-        let DOMimg = document.getElementById('canapimg');
 
-
+        //check si les valeurs ne sont pas vide pour eviter de lancer la fonction inutilement
         if (nbrArticle > 0 && choosenColor != null) {
+
+            //verification si un panier existe deja,sinon creation du panier et de l'objet contenu
             if (localStorage.getItem("storedcart") === null) {
                 createItem();
             } else {
+
+                //récupération du panier existant
                 cart = JSON.parse(localStorage.getItem("storedcart"));
-                console.log(cart);
+
+                //vérification si le panier a deja un produit ayant la meme ID et couleur,si oui récupere l'index de l'objet
                 let currentArray = cart.findIndex(f => f._id === id && f.colors === choosenColor);
-                console.log(currentArray);
+                //si un produit a été trouvé,son index vaut 0 ou plus,si rien n'a été trouvé, l'index sera de -1
                 if (currentArray >= 0){
+                    //rajoute la valeur numerique( parseInt transforme string en integer) du nombre d'article
+                    // séléctioné avec le nombre d'article déja dans le panier
                     cart[currentArray].nbrArticle = parseInt(cart[currentArray].nbrArticle) + parseInt(nbrArticle);
-                    console.log(cart[currentArray].nbrArticle);
                     localStorage.setItem("storedcart", JSON.stringify(cart));
                 } else {
+                    //crée une nouvelle entrée dans l'array du panier
                     createItem();
                 }
 
@@ -108,29 +96,26 @@ function addToCart() {
 }
 
 
+//Fonction appelée lors de la création d'objet dans le panier
 
+function createItem(){
+    let nbrArticle = document.getElementById('quantity').value;
+    let choosenColor = document.getElementById('colors').value;
+    let DOMimg = document.getElementById('canapimg');
 
+    //creation de l'objet contenant les données
+    cartItem = {
+        _id: id,
+        colors: choosenColor,
+        name: DOMtitle.innerHTML,
+        price: DOMprice.innerHTML,
+        imageUrl: DOMimg.src,
+        description: DOMdesc.innerHTML,
+        altTxt: DOMimg.alt,
+        nbrArticle: nbrArticle,
+    }
+    // push de l'objet dans l'array a mettre dans le panier
+    cart.push(cartItem);
+    localStorage.setItem("storedcart", JSON.stringify(cart));
+}
 
-
-//         fetch("http://localhost:3000/api/products/"+ id)
-//         .then(function(res){
-//             if(res.ok){
-//                 return res.json()
-//             } else {
-//                 console.log("erreur")
-//             }
-//         })
-//         .then(function(noname){
-//             let nbrArticle = document.getElementById('quantity').value;
-//             let choosenColor = document.getElementById('colors').value;
-
-//             noname && Object.assign(cartItem, noname);
-//             console.log(cartItem);
-//             console.log(nbrArticle);
-//             console.log(choosenColor);
-//         })
-//     })
-// }
-
-// a faire, ajouter le brn article et bonne couleur dans object cartItem puis stocké dans localstorage
-// verifier si cartItem existe deja et ajouté nbr item
